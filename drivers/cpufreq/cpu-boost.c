b8f3932831b3c46281c52a25d7149b1c8e4ec1f6
joshuous <joshuous@gmail.com>
2018-07-09 17:27:52 +0200

    cpu-boost: Update functions for newer Dynamic Schedtune Boost changes
    We now need to pass the functions a boost slot argument.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -56,6 +56,7 @@ module_param(input_boost_ms, uint, 0644);
 static int dynamic_stune_boost;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
+static int boost_slot;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 static unsigned int powerkey_input_boost_ms = 400;
@@ -235,7 +236,7 @@ static void do_input_boost_rem(struct work_struct *work)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
 	if (stune_boost_active) {
-		reset_stune_boost("top-app");
+		reset_stune_boost("top-app", boost_slot);
 		stune_boost_active = false;
 	}
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
@@ -264,7 +265,7 @@ static void do_input_boost(struct kthread_work *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (stune_boost_active) {
-		reset_stune_boost("top-app");
+		reset_stune_boost("top-app", boost_slot);
 		stune_boost_active = false;
 	}
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
@@ -290,7 +291,7 @@ static void do_input_boost(struct kthread_work *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Set dynamic stune boost value */
-	ret = do_stune_boost("top-app", dynamic_stune_boost);
+	ret = do_stune_boost("top-app", dynamic_stune_boost, &boost_slot);
 	if (!ret)
 		stune_boost_active = true;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
@@ -408,7 +409,7 @@ static void cpuboost_input_disconnect(struct input_handle *handle)
 {
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
-	reset_stune_boost("top-app");
+	reset_stune_boost("top-app", boost_slot);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	input_close_device(handle);
